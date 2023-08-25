8a424cbea349ad76312dd3a880c3365f35b52202
ch3lla <chukwumaemmanuella03@gmail.com>
2023-08-25 10:52:20 +0100

    fixed errors

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 #create a file in /tmp.
 
-file { '/tmp/school': 
-ensure => 'present',
-mode => '0744'
-owner => 'www-data',
-group => 'www-data',
-content => 'I love Puppet'}
+file { '/tmp/school':
+    ensure  => 'present',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+}
